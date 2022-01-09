1f5f94829bc84c5c488e12380e0b22a7d4b9ccaa
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2022-01-10 00:58:54 +0100

    #2 izmena gluposti

--- fishing-booker-frontend/src/app/app.module.ts
+++ fishing-booker-frontend/src/app/app.module.ts
@@ -1,1 +1,66 @@
-export class AppModule {}
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AdventuresComponent } from './adventures/adventures.component';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { AdventureDetailComponent } from './adventure-detail/adventure-detail.component';
+import { CottagesComponent } from './cottages/cottages.component';
+import { CottageDetailComponent } from './cottage-detail/cottage-detail.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SpecialRegistrationComponent } from './special-registration/special-registration.component';
+import { BoatsComponent } from './boats/boats.component';
+import { BoatDetailComponent } from './boat-detail/boat-detail.component';
+import { OwnedCottagesComponent } from './owned-cottages/owned-cottages.component';
+import { CottageEditComponent } from './cottage-edit/cottage-edit.component';
+import { TokenInterceptor } from './interceptor/tokenInterceptor';
+import { ClientCottageReservationsComponent } from './client/client-cottage-reservations/client-cottage-reservations.component';
+import { ClientBoatReservationsComponent } from './client/client-boat-reservations/client-boat-reservations.component';
+import { OwnedBoatsComponent } from './owned-boats/owned-boats.component';
+import { BoatEditComponent } from './boat-edit/boat-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    AdventuresComponent,
+    AdventureDetailComponent,
+    CottagesComponent,
+    CottageDetailComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ProfileComponent,
+    SpecialRegistrationComponent,
+    BoatsComponent,
+    BoatDetailComponent,
+    OwnedCottagesComponent,
+    CottageEditComponent,
+    ClientCottageReservationsComponent,
+    ClientBoatReservationsComponent,
+    OwnedBoatsComponent,
+    BoatEditComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
