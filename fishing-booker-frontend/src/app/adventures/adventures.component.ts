4684c76e2bb945685758c0b1cc1d6fa876f380bd
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2022-01-10 21:55:31 +0100

    #1 sortiranje avantura

--- fishing-booker-frontend/src/app/adventures/adventures.component.ts
+++ fishing-booker-frontend/src/app/adventures/adventures.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 import { AdventureService } from './adventure.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { AdventureService } from './adventure.service';
 export class AdventuresComponent implements OnInit {
 
   adventures: any[] = []
+  sortedData: any[] = []
 
   constructor(private _adventureService: AdventureService) { }
 
@@ -20,7 +22,31 @@ export class AdventuresComponent implements OnInit {
     this._adventureService.getAdventures().subscribe(
       adventures => {
         this.adventures = adventures;
+        this.sortedData = this.adventures.slice();
       }
     )
   }
+
+  sortData(sort: Sort) {
+    const data = this.adventures.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name': return compare(a.name, b.name, isAsc);
+        case 'address': return compare(a.address, b.address, isAsc);
+        case 'maxPeople': return compare(a.maxPeople, b.maxPeople, isAsc);
+        case 'instructor': return compare(a.instructor.email, b.instructor.email, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
