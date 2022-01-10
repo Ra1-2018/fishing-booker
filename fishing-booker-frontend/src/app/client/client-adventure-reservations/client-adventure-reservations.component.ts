f399c23cfce1f1e726ed820a15d17379fa520efa
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2022-01-10 22:43:02 +0100

    #1 sortiranje rezervacija

--- fishing-booker-frontend/src/app/client/client-adventure-reservations/client-adventure-reservations.component.ts
+++ fishing-booker-frontend/src/app/client/client-adventure-reservations/client-adventure-reservations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 import { ClientAdventuresReservationsService } from './client-adventures-reservations.service';
 
 @Component({
@@ -9,6 +10,7 @@ import { ClientAdventuresReservationsService } from './client-adventures-reserva
 export class ClientAdventureReservationsComponent implements OnInit {
 
   reservations: any[] = []
+  sortedData: any[] = []
 
   constructor(private _reservationService: ClientAdventuresReservationsService) { }
 
@@ -20,8 +22,31 @@ export class ClientAdventureReservationsComponent implements OnInit {
     this._reservationService.getReservations().subscribe(
       reservations => {
         this.reservations = reservations;
+        this.sortedData = this.reservations.slice();
       }
     )
   }
 
+  sortData(sort: Sort) {
+    const data = this.reservations.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name': return compare(a.service.name, b.service.name, isAsc);
+        case 'price': return compare(a.price, b.price, isAsc);
+        case 'reservationStartDateAndTime': return compare(a.reservationStartDateAndTime, b.reservationStartDateAndTime, isAsc);
+        case 'durationInDays': return compare(a.durationInDays, b.durationInDays, isAsc);
+        default: return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
