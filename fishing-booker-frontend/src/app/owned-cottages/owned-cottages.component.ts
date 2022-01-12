e7c5d58c68cd64b22df7c1b1cf96aceec5158dee
BogdanaZivkovic <zivkovic.ra2.2018@uns.ac.rs>
2022-01-12 11:17:28 +0100

    #2 search pipe dodat

--- fishing-booker-frontend/src/app/owned-cottages/owned-cottages.component.ts
+++ fishing-booker-frontend/src/app/owned-cottages/owned-cottages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OwnedCottagesService } from './owned-cottages.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Sort } from '@angular/material/sort';
 import { CottageService } from '../cottages/cottage.service';
 
 @Component({
@@ -10,11 +11,13 @@ import { CottageService } from '../cottages/cottage.service';
 })
 export class OwnedCottagesComponent implements OnInit {
 
+  query: '' | undefined;
+
   public readonly myFormGroup: FormGroup;
 
   cottages: any[] = []
+  sortedData: any[] = []
 
-  
   constructor(private _cottageService: OwnedCottagesService, private readonly formBuilder: FormBuilder) {
     this.myFormGroup = this.formBuilder.group({
       name: ['', Validators.required],
@@ -36,6 +39,7 @@ export class OwnedCottagesComponent implements OnInit {
     this._cottageService.getCottages().subscribe(
       cottages => {
         this.cottages = cottages;
+        this.sortedData = this.cottages.slice();
       }
     )
   }
@@ -65,4 +69,29 @@ export class OwnedCottagesComponent implements OnInit {
     });
   }
 
+  sortData(sort: Sort) {
+    const data = this.cottages.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name': return compare(a.name, b.name, isAsc);
+        case 'address': return compare(a.address, b.address, isAsc);
+        case 'roomsTotalNumber': return compare(a.roomsTotalNumber, b.roomsTotalNumber, isAsc);
+        case 'cottageOwner': return compare(a.cottageOwner.email, b.cottageOwner.email, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
 }
+  
+  function compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+
