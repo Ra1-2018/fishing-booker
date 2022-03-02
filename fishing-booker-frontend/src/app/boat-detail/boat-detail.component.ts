531d624eac09aa062d17966e6e28ccce88b9dd64
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2022-03-02 10:47:37 +0100

    #1 pretplata/odjava za brodove

--- fishing-booker-frontend/src/app/boat-detail/boat-detail.component.ts
+++ fishing-booker-frontend/src/app/boat-detail/boat-detail.component.ts
@@ -13,15 +13,20 @@ export class BoatDetailComponent implements OnInit {
   boat: any;
   errorMessage = '';
   id: number|undefined;
+  subscriptions:any[] = []
 
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private boatDetailService: BoatDetailService) { }
+              private boatDetailService: BoatDetailService,
+              public readonly loginService: LoginService) { }
 
   ngOnInit(): void {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
     if (this.id) {
       this.getBoat(this.id);
+    }
+    if(this.loginService.isLoggedIn && this.loginService.userType == 'CLIENT') {
+      this.getSubscriptions();
     }
   }
 
@@ -30,5 +35,36 @@ export class BoatDetailComponent implements OnInit {
       next: boat => this.boat = boat,
       error: err => this.errorMessage = err
     })
+  }
+
+  subscribe(): void {
+    this.boatDetailService.subscribe(this.id as number).subscribe({
+      next: () => {
+        this.getSubscriptions();
+      }
+    });
+  }
+
+  isSubscribed(): boolean {
+    for(let subscription of this.subscriptions) {
+      if(subscription.id == this.boat.id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getSubscriptions() {
+    this.boatDetailService.getSubscriptions().subscribe(
+      subscriptions => {this.subscriptions = subscriptions;}
+    )
+  }
+
+  unsubscribe() {
+    this.boatDetailService.unsubscribe(this.id as number).subscribe({
+      next: () => {
+        this.getSubscriptions();
+      }
+    });
   }
 }
