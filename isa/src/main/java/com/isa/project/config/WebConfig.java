58fe07a0d6a0a08cd302437f5b397c064d0731a0
nikolaivanovic <ivanovic.ra1.2018@uns.ac.rs>
2022-05-31 17:30:56 +0200

    cors mapping

--- isa/src/main/java/com/isa/project/config/WebConfig.java
+++ isa/src/main/java/com/isa/project/config/WebConfig.java
@@ -10,5 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**").allowedOrigins("https://fishing-booker-client.herokuapp.com");
     }
 }
