43743f80cd0dbf252fb28b47fa52ae2e3ab86079
lenche <lenkaisidora.aleksic@gmail.com>
2022-06-06 16:54:46 +0200

    #3 transakcije za zahteve brisanja naloga i zalbi

---
+++ isa/src/main/java/com/isa/project/service/ResponseToDeletionTransactionService.java
@@ -0,0 +1,34 @@
+package com.isa.project.service;
+
+import com.isa.project.model.ResponseToDeletionRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ResponseToDeletionTransactionService {
+
+    @Autowired
+    private ResponseToDeletionRequestService responseService;
+
+    @Autowired
+    private  DeletionRequestService deletionService;
+
+    @Autowired
+    private AppUserService appUserService;
+
+    @Transactional
+    public void responseToApproveDeletionTransactional(Long requestId, Long userId) throws OptimisticLockingFailureException {
+        deletionService.remove(requestId);
+        appUserService.remove(userId);
+    }
+
+    @Transactional
+    public void responseToDeclineDeletionTransactional(ResponseToDeletionRequest response, Long requestId) throws OptimisticLockingFailureException{
+        deletionService.remove(requestId);
+        responseService.save(response);
+
+    }
+}
